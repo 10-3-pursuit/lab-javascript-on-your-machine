5dc4a232db61261b4b0c5f3629883d064761c180
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2023-09-11 15:29:29 -0400

    completed calculator and tests passed in index.js . package was also updated,it had 5 vulnerabilities

--- index.js
+++ index.js
@@ -4,7 +4,34 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+
+let myArray =process.argv.slice(2);
+if(myArray.length<1){
+    return "No operation provided...";
+}
+
+const[operation,...numbers]= myArray;
+if(numbers.length ===0){
+    return "No numbers provided...";
+}
+
+if(!["plus" , "minus"].includes(operation)){
+    return `Invalid operation: ${operation}`;
+
+}
+let total = parseInt(numbers[0]);
+for(let number of numbers.slice(1)){
+    if(operation === "plus"){
+        total+= parseInt(number)
+    }else if(operation === "minus"){
+        total-= parseInt(number);
+    }
+}
+return total;
+}
+
+console.log(calculator());
 
 // Don't change anything below this line.
 module.exports = calculator;
